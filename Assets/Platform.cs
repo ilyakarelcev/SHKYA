ce8f0024eee1ac31ba095631b15568128ba0e066
ilyakarelcev <53k@mail.ru>
2021-06-07 05:51:26 +0500

    Платформы

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Platform : MonoBehaviour {
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        Player player = collision.rigidbody.GetComponent<Player>();
+        if (player) {
+            if (Vector2.Angle(collision.contacts[0].normal, Vector2.up) < 10f) {
+                Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal);
+                player.transform.parent = transform;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionExit2D(Collision2D collision) {
+        Player player = collision.rigidbody.GetComponent<Player>();
+        if (player) {
+            player.transform.parent = null;
+        }
     }
+
 }
